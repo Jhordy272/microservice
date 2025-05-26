65e84df54121e5b28ddb2b748ba7bf7612ed9621
Jhordy272 <jaagatonp@udistrital.edu.co>
2025-05-26 10:06:35 -0500

    Change to singleton the db connection

--- app/db/Database_Connection_ORM.py
+++ app/db/Database_Connection_ORM.py
@@ -1,37 +1,46 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import declarative_base
-import os
+from sqlalchemy.orm import sessionmaker, declarative_base
 from urllib.parse import quote_plus
 from dotenv import load_dotenv
-from pathlib import Path
+import os
 
 load_dotenv()
 
 Base = declarative_base()
 
 class DatabaseConnectionORM:
+    _engine = None
+    _Session = None
+
     def __init__(self):
-        self.host = os.getenv('DB_HOST')
-        self.user = os.getenv('DB_USER')
-        self.password = os.getenv('DB_PASSWORD')
-        self.db = os.environ.get('DB_DB')
-        try:
-            self.engine = create_engine(
-                f'mysql+pymysql://{self.user}:{quote_plus(self.password)}@{self.host}/{self.db}?charset=utf8mb4'
-            )
-        except Exception as e:
-            print(f'Error connecting: {e}')
+        if not DatabaseConnectionORM._engine:
+            host = os.getenv('DB_HOST')
+            user = os.getenv('DB_USER')
+            password = os.getenv('DB_PASSWORD')
+            db = os.getenv('DB_DB')
+
+            try:
+                DatabaseConnectionORM._engine = create_engine(
+                    f'mysql+pymysql://{user}:{quote_plus(password)}@{host}/{db}?charset=utf8mb4',
+                    pool_size=10,
+                    max_overflow=20,
+                    pool_timeout=30,
+                    pool_recycle=1800,
+                    pool_pre_ping=True
+                )
+                DatabaseConnectionORM._Session = sessionmaker(bind=DatabaseConnectionORM._engine)
+            except Exception as e:
+                print(f'Error connecting to the database: {e}')
 
     def get_base(self):
         return Base
     
     def get_engine(self):
-        return self.engine
+        return DatabaseConnectionORM._engine
 
-    def get_session(self): 
-        Session = sessionmaker(bind=self.engine)
-        return Session()
-    
+    def get_session(self):
+        return DatabaseConnectionORM._Session()
+
     def close(self):
-        self.engine.dispose()
+        if DatabaseConnectionORM._engine:
+            DatabaseConnectionORM._engine.dispose()
